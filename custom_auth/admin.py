3e7ef4b01e6956ff307f21b481a9fac9b83001ca
Rahul0700 <antorahul070@gmail.com>
2021-10-18 08:58:39 +0530

    Bugs and fixes models and migrations

--- custom_auth/admin.py
+++ custom_auth/admin.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from django.contrib import admin
 from django_rest_passwordreset.models import ResetPasswordToken
 from django.contrib.sites.models import Site
 from rest_framework.authtoken.models import TokenProxy
 
+admin.site.unregister(User)
 admin.site.unregister(Group)
 admin.site.unregister(ResetPasswordToken)
 admin.site.unregister(Site)
